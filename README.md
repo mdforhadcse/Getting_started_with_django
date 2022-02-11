5fa3dd1cee6679f976908462821dc7b0776125d6
Md. Forhad Hossain <forhad14@cse.pstu.ac.bd>
2022-02-11 10:21:20 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Getting_started_with_Django
+ In this repo, I am learning django with doing different project
---
+++ storefront/playground/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# request -> response
+# request handler
+# action
+
+def say_hello(request):
+    #pull data from db
+    # transform
+    # send email
+    # return HttpResponse('Hello world') 
+    return render(request, 'hello.html', {'name': 'Mosh'})
